559c430450e7476af8ed003caed383180f7166a6
Richard Opiyo <romenyo@dewcis.com>
2022-05-10 00:36:18 +0300

    implement get reuest in the app and configure test

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('Hello Richard');
+const express = require('express');
+const app = express();
+
+const myNumbers = [
+{ id: 1, name: 'one' },
+{ id: 2, name: 'Two' },
+{ id: 3,  name: 'Three' }
+]
+
+app.get('/', (req, res) => {
+  res.send('I am in the game!');
+});
+
+ app.get('/api/number/:id', (req, res) => {
+    const numId = myNumbers.find( c => c.id === parseInt(req.params.id));
+
+    if(!numId) res.status(404).send('A wrong one!');
+
+    res.send(numId);
+ })
+
+const port = process.env.PORT || 3000;
+
+let server = app.listen(port, () => {
+	console.log(`Connected on port ${port}`);
+});
+
+module.exports = server;
